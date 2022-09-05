04d5817ce7a15c673e841755838375aea7e73e3b
Gleb Dianov <gleb.dianov@protonmail.com>
2022-09-05 19:10:14 +0200

    helicopter money

--- index.js
+++ index.js
@@ -11,19 +11,35 @@ const wallet = new Keypair()
 const publicKey = new PublicKey(wallet._keypair.publicKey)
 const secretKey = wallet._keypair.secretKey
 
-console.log(publicKey);
+console.log(publicKey.toString());
+
+const network = 'devnet'
 
 const getWalletBalance = async () => {
     try {
-        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const connection = new Connection(clusterApiUrl(network), 'confirmed')
         const walletBalance = await connection.getBalance(publicKey)
-        console.log(`Wallet balance: ${walletBalance}`);
+        console.log(`Wallet balance: ${walletBalance / LAMPORTS_PER_SOL} SOL`);
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+const airdropSol = async () => {
+    try {
+        const connection = new Connection(clusterApiUrl(network), 'confirmed')
+        const fromAirdropSig = await connection.requestAirdrop(publicKey, 1 * LAMPORTS_PER_SOL)
+        await connection.confirmTransaction(fromAirdropSig)
     } catch (err) {
         console.error(err)
     }
 }
 
 const main = async () => {
+    await getWalletBalance()
+
+    await airdropSol()
+
     await getWalletBalance()
 }
 
